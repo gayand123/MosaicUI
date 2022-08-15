bbdb10be2367172202a680b92a541a5c3806c99c
Gayand <gayandunuwila@gmail.com>
2022-08-15 21:49:51 +0530

    added customer test files

---
+++ MosaicUIAutomationProject/src/test/java/com/mosaic/pages/adminModule/AdminCustomerPage.java
@@ -0,0 +1,146 @@
+package com.mosaic.pages.adminModule;
+
+import com.google.gson.internal.bind.util.ISO8601Utils;
+import com.mosaic.util.TestBase;
+import com.mosaic.util.adminModule.ElementsCustomers;
+import org.apache.commons.lang3.StringUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class AdminCustomerPage extends TestBase {
+    @FindBy(xpath = ElementsCustomers.lblCustomers)
+    private WebElement lblCustomers;
+    @FindBy(xpath = ElementsCustomers.btnNextPageArrow)
+    private WebElement btnNextPageArrow;
+    @FindBy(xpath = ElementsCustomers.drpDownResultPerPage)
+    private WebElement drpDownResultPerPage;
+    @FindBy(xpath = ElementsCustomers.drpDownResultPerPage30)
+    private WebElement drpDownResultPerPage30;
+    @FindBy(xpath = ElementsCustomers.drpDownResultPerPage100)
+    private WebElement drpDownResultPerPage100;
+    public String getCustomerText() {
+        return getElementText(lblCustomers);
+    }
+
+    public void clickNextPageInCustomers() {
+        if (isElementEnabled(btnNextPageArrow) == true) {
+            clickOnElement(btnNextPageArrow);
+        } else
+            System.out.println("cannot click");
+    }
+
+    public int getRowCountInCustomers(double resultPerPage) throws InterruptedException {
+        double customerCount = getCustomerCount();
+        System.out.println("customerCount "+customerCount);
+        System.out.println("resultPerPage"+resultPerPage);
+        double pageCount =customerCount/resultPerPage;
+        System.out.println("pageCount" + pageCount);
+       int correctPageCount= (int) Math.ceil(pageCount);
+        System.out.println("correctPageCount "+correctPageCount);
+        int tempCustomerCount = 0;
+        int rowCount = 0;
+        //   int rowCount = driver.findElements(By.xpath(ElementsCustomers.tblRowCounts)).size();
+        for (int temp = 0; temp <= correctPageCount; temp++) {
+           tempCustomerCount = driver.findElements(By.xpath(ElementsCustomers.tblRowCounts)).size();
+            rowCount = rowCount + tempCustomerCount;
+            if (isElementEnabled(btnNextPageArrow) == true) {
+                clickOnElement(btnNextPageArrow);
+            }
+            scrollDown();
+            System.out.println("rowCount  " + rowCount);
+        }
+        System.out.println("total row Count  " + rowCount);
+        return rowCount;
+    }
+
+    public int getCustomerCount() {
+        String customerCount = replacement(getCustomerText(), "Customers ");
+        String result = StringUtils.substringBetween(customerCount, "(", ")");
+        System.out.println("result " + result);
+        int intcustomerCount = 0;
+        intcustomerCount = Integer.parseInt(result);
+
+        System.out.println("customerCount " + intcustomerCount);
+        return intcustomerCount;
+    }
+
+public void clickResultPerpage(){
+    moveAndClick(drpDownResultPerPage);
+}
+
+public void clickResultPerPageValue(int resultPerPage){
+        if(resultPerPage ==30) {
+            moveAndClick(drpDownResultPerPage30);
+        } if (resultPerPage==100){
+            moveAndClick(drpDownResultPerPage100);
+    }
+}
+
+    public int getRowCount30InCustomers(double resultPerPage) throws InterruptedException {
+        double customerCount = getCustomerCount();
+        System.out.println("customerCount "+customerCount);
+        System.out.println("resultPerPage"+resultPerPage);
+        double pageCount =customerCount/resultPerPage;
+        System.out.println("pageCount" + pageCount);
+        int correctPageCount= (int) Math.ceil(pageCount);
+        System.out.println("correctPageCount "+correctPageCount);
+        int tempCustomerCount = 0;
+        int rowCount = 0;
+        //   int rowCount = driver.findElements(By.xpath(ElementsCustomers.tblRowCounts)).size();
+        for (int temp = 0; temp <= correctPageCount; temp++) {
+            tempCustomerCount = driver.findElements(By.xpath(ElementsCustomers.tblRowCounts)).size();
+            rowCount = rowCount + tempCustomerCount;
+            if (isElementEnabled(btnNextPageArrow) == true) {
+                clickOnElement(btnNextPageArrow);
+            }
+            scrollDown();
+            System.out.println("rowCount  " + rowCount);
+        }
+        System.out.println("total row Count  " + rowCount);
+        return rowCount;
+    }
+
+
+    public boolean isSpecificEmailAvailableInCustomersList(double resultPerPage,String customerEmail) throws InterruptedException {
+
+        double customerCount = getCustomerCount();
+        System.out.println("customerCount "+ customerCount);
+        System.out.println("resultPerPage"+ resultPerPage);
+        double pageCount = customerCount/ resultPerPage;
+        System.out.println("pageCount" + pageCount);
+        int correctPageCount= (int) Math.ceil(pageCount);
+        System.out.println("correctPageCount "+ correctPageCount);
+        int tempCustomerCount = 0;
+        int rowCount = 0;
+        boolean isEmailavailable = false;
+        //   int rowCount = driver.findElements(By.xpath(ElementsCustomers.tblRowCounts)).size();
+        for (int temp = 0; temp <= correctPageCount; temp++) {
+            tempCustomerCount = driver.findElements(By.xpath(ElementsCustomers.lblCustomersEmail)).size();
+            System.out.println("tempCustomerCount "+tempCustomerCount);
+
+            for ( int x=0; x<tempCustomerCount; x++) {
+           String email = driver.findElements(By.xpath(ElementsCustomers.lblCustomersEmail)).get(x).getText();
+                System.out.println("email "+ email);
+                if(email.contains(customerEmail)){
+                    isEmailavailable =  true;
+                    break;
+                }
+
+            }
+            rowCount = rowCount + tempCustomerCount;
+            if (isElementEnabled(btnNextPageArrow) == true) {
+                clickOnElement(btnNextPageArrow);
+            }
+            scrollDown();
+            System.out.println("rowCount  " + rowCount);
+        }
+        System.out.println("total row Count  " + rowCount);
+        return isEmailavailable;
+    }
+
+
+
+
+
+}
--- MosaicUIAutomationProject/src/test/java/com/mosaic/util/adminModule/ElementsMenu.java
+++ MosaicUIAutomationProject/src/test/java/com/mosaic/util/adminModule/ElementsMenu.java
@@ -10,6 +10,10 @@ public class ElementsMenu {
     // Admin Menu Elements
     public static final String btnMenu ="//*[@id=\"__next\"]/header/div/div[1]/button/span[1]/img";
     public static final String btnLoyalty="//*[text()='Loyalty']";
+    public static final String btnCustomers ="//*[text()='Customers']";
+
     public static final String btnOffers ="//*[@id=\"__next\"]/div/div[1]/div/div/ul/a[2]/div/div[2]/p";
     public static final String lblRedeemPoints ="//*[@id=\"__next\"]/div/div[2]/div/div/div[1]/div/h5";
+
+    public static final String btnCloseMenuForm ="//button[@aria-label='close']";
 }
