e7a2354250050ae66fdda2fcbbe0087f62f66d16
Isuri-Prabuddhini <pgisuriprabuddhini@gmai.com>
2022-09-19 14:57:18 +0530

    order now button new changes

--- MosaicUIAutomationProject/src/test/java/com/mosaic/pages/storeFrontModule/StorefrontCartOffersPage.java
+++ MosaicUIAutomationProject/src/test/java/com/mosaic/pages/storeFrontModule/StorefrontCartOffersPage.java
@@ -1,2 +1,17 @@
-package com.mosaic.pages.storeFrontModule;public class StorefrontCartOffersPage {
+package com.mosaic.pages.storeFrontModule;
+
+import com.mosaic.util.TestBase;
+import com.mosaic.util.storeFrontModule.ElementsCartOffers;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class StorefrontCartOffersPage extends TestBase {
+
+    @FindBy(xpath = ElementsCartOffers.btnOrderNow)
+    private WebElement btnOrderNow;
+
+    public void clickBtnOrderNow() {
+
+        clickOnElement(btnOrderNow);
+    }
 }
