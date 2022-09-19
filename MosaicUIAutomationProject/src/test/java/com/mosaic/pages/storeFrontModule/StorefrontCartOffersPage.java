80b01783248032c6761d402eef88d76a5f6cee0d
Isuri-Prabuddhini <pgisuriprabuddhini@gmai.com>
2022-09-19 12:36:47 +0530

    Storefront cart

---
+++ MosaicUIAutomationProject/src/test/java/com/mosaic/pages/storeFrontModule/StorefrontCartOffersPage.java
@@ -0,0 +1,20 @@
+package com.mosaic.pages.storeFrontModule;
+
+import com.mosaic.util.TestBase;
+import com.mosaic.util.storeFrontModule.ElementsCartOffers;
+import com.mosaic.util.storeFrontModule.ElementsSignIn;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class StorefrontCartOffersPage extends TestBase {
+
+    @FindBy(xpath = ElementsCartOffers.btnOrderNow)
+    private WebElement btnOrderNow;
+
+    public void clickBtnOrderNow() {
+
+        clickOnElement(btnOrderNow);
+    }
+
+
+}
---
+++ MosaicUIAutomationProject/src/test/java/com/mosaic/test/storeFrontModule/StorefrontCartOffersTest.java
@@ -0,0 +1,27 @@
+package com.mosaic.test.storeFrontModule;
+
+import com.mosaic.pages.storeFrontModule.StorefrontCartOffersPage;
+import com.mosaic.pages.storeFrontModule.StorefrontRewardsPage;
+import com.mosaic.util.TestBase;
+import org.apache.log4j.lf5.viewer.configure.ConfigurationManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class StorefrontCartOffersTest extends TestBase {
+
+    @BeforeClass
+    public void storefrontUserLogin() throws InterruptedException {
+        storeFrontUserLogin();
+    }
+
+    @Test
+    public void checkOfferCount() throws InterruptedException {
+        StorefrontCartOffersPage storefrontCartOffersPage = PageFactory.initElements(driver, StorefrontCartOffersPage.class);
+        storefrontCartOffersPage.clickBtnOrderNow();
+    }
+
+}
