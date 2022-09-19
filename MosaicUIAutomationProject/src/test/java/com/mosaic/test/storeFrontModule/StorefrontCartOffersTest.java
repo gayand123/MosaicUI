6424b0d5dacbcb1d7d35c92080d79780908be0b6
Gayand <gayandunuwila@gmail.com>
2022-09-19 15:20:04 +0530

    update code

--- MosaicUIAutomationProject/src/test/java/com/mosaic/test/storeFrontModule/StorefrontCartOffersTest.java
+++ MosaicUIAutomationProject/src/test/java/com/mosaic/test/storeFrontModule/StorefrontCartOffersTest.java
@@ -1,10 +1,15 @@
 package com.mosaic.test.storeFrontModule;
 
 import com.mosaic.pages.storeFrontModule.StorefrontCartOffersPage;
+import com.mosaic.pages.storeFrontModule.StorefrontRewardsPage;
 import com.mosaic.util.TestBase;
-import org.junit.Test;
+import org.apache.log4j.lf5.viewer.configure.ConfigurationManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 public class StorefrontCartOffersTest extends TestBase {
 
@@ -15,9 +20,11 @@ public class StorefrontCartOffersTest extends TestBase {
 
     @Test
     public void checkOfferCount() throws InterruptedException {
+
         StorefrontCartOffersPage storefrontCartOffersPage = PageFactory.initElements(driver, StorefrontCartOffersPage.class);
         // driver.switchTo().alert().accept();
         storefrontCartOffersPage.clickBtnOrderNow();
 
     }
+
 }
